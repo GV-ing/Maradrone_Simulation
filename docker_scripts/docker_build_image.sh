f7271b2cd655d665aa7312267bc3c75a87e45c9c
GV-ing <g.vestri2904@gmail.com>
2026-08-08 00:55:43 +0200

    commit

--- docker_scripts/docker_build_image.sh
+++ docker_scripts/docker_build_image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Spostati nella cartella contenente lo script
-cd "$(dirname "$0")"
+# Spostati nella directory principale del progetto (un livello sopra lo script)
+cd "$(dirname "$0")/.."
 
 echo "Costruzione dell'immagine Docker per maradrone..."
-docker build -t maradrone_image .
+# Usa il punto (.) per prendere tutta la cartella principale come contesto,
+# e specifica dove trovare il Dockerfile con -f
+docker build -t maradrone_image -f docker_scripts/Dockerfile .
 echo "Build completata con successo!"
